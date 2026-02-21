8118b35d87ac7fdd293c947d8ecb333f863b0abd
sbahn <sbahneuclidsoft@gmail.com>
2026-02-21 09:28:55 +0900

    업데이트 내역

--- updates.js
+++ updates.js
@@ -1,5 +1,11 @@
 const updateHistory = [
     {
+        version: 'v1.0.1',
+        date: '2026-02-20',
+        summary: 'UI/UX 개선, 밸런스 조정 및 버그 수정이 진행되었습니다.',
+        file: 'update/v1.0.1.md'
+    },
+    {
         version: 'v1.0.0(베타버전)',
         date: '2026-02-19',
         summary: '베타 버전이 출시되었습니다. 자세한 내용을 보려면 클릭하세요.',
